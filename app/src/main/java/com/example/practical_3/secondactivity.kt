dda3ca20e0aa9ed95cd0be03f8c9f27804c7a940
ASUS <niralip1810@gmail.com>
2022-09-14 10:20:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/practical_3/secondactivity.kt
@@ -0,0 +1,17 @@
+package com.example.practical_3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class secondactivity : AppCompatActivity() {
+    lateinit var tv1:TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_secondactivity)
+        tv1=findViewById(R.id.textview1)
+        val intent1=intent
+        val displayText=intent1.getStringExtra("MainActivity").toString()
+        tv1.text=displayText
+    }
+}
